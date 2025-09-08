867018c0e97f94f11984b95ca63f9e9fa9438283
Caleb Cook <bmxcaleb@gmail.com>
2025-09-08 06:10:59 -0500

    run/walk/jump implemented
    Basic 3d controller implemented for CONTROLLER ONLY aat the moment, have mouse + keyboard controls commented out

---
+++ scenes/entities/player/player.gd
@@ -0,0 +1,48 @@
+extends CharacterBody3D
+
+#jump
+@export var jump_height : float = 2.25
+@export var jump_time_to_peak : float = .3
+@export var jump_time_to_descent : float = .4
+
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+@export var base_speed := 4.0
+@export var run_speed := 8.0
+
+var movement_input := Vector2.ZERO
+
+@onready var camera = $CameraController/Camera3D
+
+func _physics_process(delta: float) -> void:
+	movement_logic(delta)
+	jump_logic(delta)
+	
+	move_and_slide()
+
+func jump_logic (delta) -> void:
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -jump_velocity
+	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
+	velocity.y -= gravity * delta
+
+func movement_logic (delta) -> void:
+	movement_input = Input.get_vector("left","right", "forward", "backward").rotated(-camera.global_rotation.y)
+	var vel_2d = Vector2(velocity.x, velocity.z)
+	
+	if movement_input != Vector2.ZERO:
+		if Input.is_action_pressed("run"):
+			vel_2d += movement_input * run_speed * delta
+			vel_2d = vel_2d.limit_length(run_speed)
+		else:
+			vel_2d += movement_input * base_speed * delta
+			vel_2d = vel_2d.limit_length(base_speed)
+		velocity.x = vel_2d.x
+		velocity.z = vel_2d.y
+	else:
+		vel_2d = vel_2d.move_toward(Vector2.ZERO, base_speed * 4.0 * delta)
+		velocity.x = vel_2d.x
+		velocity.z = vel_2d.y
